0ae5690afd7f70a0ad2b6d33ae1b8bc2b1a7e2ad
박진억 <cramis@gmail.com>
2018-11-02 00:54:05 +0900

    first commit

---
+++ src/main/java/com/cramislab/springwebservice/web/WebRestController.java
@@ -0,0 +1,20 @@
+package com.cramislab.springwebservice.web;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld!!";
+    }
+}
