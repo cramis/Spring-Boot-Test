538beadf13e8c143380c9cb613766813426fc556
박진억 <cramis@gmail.com>
2018-11-02 13:08:11 +0900

    Posts 간단 구현

--- src/main/java/com/cramislab/springwebservice/web/WebRestController.java
+++ src/main/java/com/cramislab/springwebservice/web/WebRestController.java
@@ -1,6 +1,8 @@
 package com.cramislab.springwebservice.web;
 
 
+import com.cramislab.springwebservice.domain.posts.DTO.PostsSaveRequestDto;
+import com.cramislab.springwebservice.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.Arrays;
 
 @RestController
 public class WebRestController {
 
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
+
         return "HelloWorld!!";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
